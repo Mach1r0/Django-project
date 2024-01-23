cacf34d5b6de2d4a3539f6ab0d42a5448325a06f
mach1r0 <segundacontafd@gmail.com>
2024-01-23 11:55:54 -0300

    Added dbsqlite and making library to view

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 
+dados = {
+    1: {"nome": "Nebulosa de carina",
+        "legenda": "webbtelecope.org / NASA / James webb"},
+    2: {"nome": "Galaxia NGC 1079",
+        "legenda": "nasa.org  / ORG",}
+}
+
 def index(request): 
-    return render(request, 'galeria/index.html')
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
