e0acc0d4edb8dbfbf7ab8b01f134eb829c3d1921
mach1r0 <segundacontafd@gmail.com>
2024-01-24 12:21:16 -0300

    update the admin page

--- galeria/views.py
+++ galeria/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
-
-dados = {
-    1: {"nome": "Nebulosa de carina",
-        "legenda": "webbtelecope.org / NASA / James webb"},
-    2: {"nome": "Galaxia NGC 1079",
-        "legenda": "nasa.org  / ORG",}
-}
+from galeria.models import Fotografia 
+from django.shortcuts import render, get_object_or_404
 
 def index(request): 
-    return render(request, 'galeria/index.html', {"cards": dados})
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id  )
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
