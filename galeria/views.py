eb59b7cff611c50e9e6551188b3023f7c8234ac7
mach1r0 <segundacontafd@gmail.com>
2024-01-29 09:50:12 -0300

    modify the admin interface

--- galeria/views.py
+++ galeria/views.py
@@ -4,7 +4,7 @@ from galeria.models import Fotografia
 from django.shortcuts import render, get_object_or_404
 
 def index(request): 
-    fotografias = Fotografia.objects.all()
+    fotografias = Fotografia.objects.filter(publicada=True)
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request, foto_id):
